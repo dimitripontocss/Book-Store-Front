d035d724d2b3bfb695cc39d7d3e7c0f1d96e81f0
Dimitri <assis.dimitri@engenharia.ufjf.br>
2022-07-06 11:41:19 -0300

    Login e sign-up funcionando usando server do localhost

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,15 +1,49 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useContext,useState } from "react";
 import styled from "styled-components";
+import axios from "axios";
+
+import UserContext from "../Context/userContext.js";
 
 export default function Login(){
+
+    const navigate = useNavigate();
+
+    const{ setToken,setUsername } = useContext(UserContext);
+    const [email, setEmail] = useState("");
+	const [senha, setSenha] = useState("");
+
+    function login(event){
+        event.preventDefault();
+        const body = {
+            email: email,
+            password: senha
+        }
+        const promise = axios.post(
+            "http://localhost:5001/login",
+            body
+          );
+          promise.then((response)=> 
+          {
+            setToken(response.data.token);
+            setUsername(response.data.name);
+            navigate("/");
+          })
+          .catch((e)=>
+            {
+                alert(e.response.data);  
+            })
+    }
 
     return(
         <Container>
             <Content>
                 <h1>BookStore</h1>
-                <Input placeholder="E-mail"  type="email" required></Input>
-                <Input placeholder="Senha" type="password" required></Input>
-                <Button>Entrar</Button>
+                <form onSubmit={login}>
+                    <Input placeholder="E-mail"  type="email" required value={email} onChange={e => setEmail(e.target.value)}></Input>
+                    <Input placeholder="Senha" type="password" required value={senha} onChange={e => setSenha(e.target.value)}></Input>
+                    <Button>Entrar</Button>
+                </form>
                 <Link to="/cadastro">Não tem uma conta? Cadastre-se!</Link>          
             </Content>
         </Container>
@@ -41,6 +75,12 @@ const Content = styled.div`
     background-color: #BABD8D;
     border-radius: 20px;
     box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.2);
+    form{
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
 `
 
 const Input = styled.input`
--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,17 +1,47 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 import styled from "styled-components";
+import axios from "axios";
 
 export default function SignUp(){
+
+    const navigate = useNavigate()
+
+    const [nome, setNome] = useState("");
+    const [email, setEmail] = useState("");
+	const [senha, setSenha] = useState("");
+	const [senha2, setSenha2] = useState("");
+
+    function signUp(event){
+        event.preventDefault();
+        const body = {
+            email: email,
+            name: nome,
+            password: senha,
+            passwordConfirmation: senha2
+        }
+        const promise = axios.post(
+            "http://localhost:5001/sign-up",
+            body
+          );
+          promise.then((a)=> {alert(a.data);navigate("/login")})
+          .catch((e)=>
+            {
+                alert(e.response.data);  
+            })
+    }
 
     return(
         <Container>
             <Content>
                 <h1>BookStore</h1>
-                <Input placeholder="Nome"  type="text" required></Input>
-                <Input placeholder="E-mail"  type="email" required></Input>
-                <Input placeholder="Senha" type="password" required></Input>
-                <Input placeholder="Confirme a senha" type="password" required></Input>
-                <Button>Cadastrar</Button>
+                <form onSubmit={signUp}>
+                    <Input placeholder="Nome"  type="text" required value={nome} onChange={e => setNome(e.target.value)}></Input>
+                    <Input placeholder="E-mail"  type="email" required value={email} onChange={e => setEmail(e.target.value)}></Input>
+                    <Input placeholder="Senha" type="password" required value={senha} onChange={e => setSenha(e.target.value)}></Input>
+                    <Input placeholder="Confirme a senha" type="password" required value={senha2} onChange={e => setSenha2(e.target.value)}></Input>
+                    <Button>Cadastrar</Button>
+                </form>
                 <Link to="/login">Já tem uma conta? Entre agora!</Link>          
             </Content>
         </Container>
@@ -43,6 +73,12 @@ const Content = styled.div`
     background-color: #BABD8D;
     border-radius: 20px;
     box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.2);
+    form{
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
 `
 
 const Input = styled.input`
