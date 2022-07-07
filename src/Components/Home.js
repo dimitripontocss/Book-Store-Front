6ac702460bbc48dfbf9a68ab40e4fb44c325e0dd
Dimitri <assis.dimitri@engenharia.ufjf.br>
2022-07-07 15:55:53 -0300

    Tela do carrinho começando a ser implementada e pequenas mudanças feitas no header da pagina principal

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,28 +1,97 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { useContext,useState } from "react";
+
+import UserContext from "../Context/userContext";
 
 export default function Home(){
 
+    const{ token,username } = useContext(UserContext);
 
-    
+    const [menu,setMenu] = useState(false);
 
     return(
             <Content>
                 <Header>
                     <h1>BookStore</h1>
-                    <Input placeholder="Pesquisar"></Input>          
-                    
-                    <Link to="/login">Auth</Link>
-                    <Link to="">Cart</Link>
+                    <Input placeholder="Pesquisar"></Input>    
+                    <User username={username} token={token} setMenu={setMenu} menu={menu}/>              
+                    <MenuUser menu={menu} setMenu={setMenu}/>                       
                 </Header>
             </Content>
     )
 }
 
+function User({username,token,setMenu,menu}){
+    return(
+        <>
+        {
+            !token ? <Link to="/login"><p style={{color:"#ffffff",fontSize:"12px",fontWeight:700,textShadow:"1px 1px 2px #7C6A0A", textAlign:"center"}}>Clique aqui e<br/> faça seu Login!</p></Link> 
+            :
+            <UserArea>
+                <p style={{color:"#ffffff",fontSize:"18px",fontWeight:700,marginRight:10}}>Olá {username}!</p>
+                {
+                    menu ? <></> : <div onClick={()=> {setMenu(true)}}><ion-icon name="caret-down-outline" style={{color:"#ffffff"}}></ion-icon></div>
+                }
+            </UserArea>
+        }
+        </>
+    )
+    }
+
+function MenuUser({menu,setMenu}){
+    return(
+        <>
+        {
+            menu ?  <PopUp>
+                        <Link to="/carrinho"><p>Ir para o Carrinho!</p></Link>
+                        <div onClick={()=>alert("Não foi implementado ainda!")}><p>Sair</p></div>
+                        <div onClick={()=>{setMenu(false)}}><ion-icon name="caret-up-outline" style={{color:"#ffffff"}}></ion-icon></div>
+                    </PopUp>
+                    :
+                    <></>
+        }
+        </>
+    )
+}
+
+const PopUp = styled.div`
+position: fixed;
+right: 2.2%;
+top: 60px;
+
+width: 165px;
+height: fit-content;
+
+border-radius: 7px;
+background-color: #BABD8D;
+
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+text-align: center;
+
+    p{
+        font-size: 14px;
+        color: #ffffff;
+    }
+
+`
+
+const UserArea = styled.div`
+display: flex;
+align-items: center;
+`
+
 const Content = styled.div`
 `
 
 const Header = styled.div`
+
+    position: fixed;
+    top: 0;
+    left: 0;
     width: 100%;
     background-color: #BABD8D;
     height: 60px;
